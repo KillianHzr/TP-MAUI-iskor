70efc4855f0ccd02a85454df279bf1c427a35fc8
KillianHzr <killianherzer@gmail.com>
2024-03-28 16:14:43 +0100

    ADD [parties] Ajout modification

--- src/i-Skör/Views/AjoutPartieView.xaml.cs
+++ src/i-Skör/Views/AjoutPartieView.xaml.cs
@@ -61,6 +61,89 @@ namespace i_Skör.Views
             await DisplayAlert(success ? "Succès" : "Erreur", message, "OK");
         }
 
-        // TODO: Implementer les méthodes OnModifierPartieClicked et OnSupprimerPartieClicked
+        private async void OnSupprimerPartieClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is Partie partie)
+            {
+                bool confirmDelete = await DisplayAlert("Confirmation", "Voulez-vous vraiment supprimer cette partie ?", "Oui", "Non");
+                if (confirmDelete)
+                {
+                    viewModel.SupprimerPartie(partie);
+                }
+            }
+        }
+
+        private async void OnModifierPartieClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is Partie partie)
+            {
+                DateTime nouvelleDate = partie.Date;
+                string nouvelleDateString = partie.Date.ToString("dd/MM/yyyy");
+                Equipe nouvelleEquipeA = partie.EquipeA;
+                int nouveauScoreA = partie.ScoreA;
+                Equipe nouvelleEquipeB = partie.EquipeB;
+                int nouveauScoreB = partie.ScoreB;
+
+                // Sélectionner une nouvelle date
+                nouvelleDateString = await DisplayPromptAsync("Modifier Partie", "Entrez la nouvelle date (format jj/mm/aaaa) :", initialValue: nouvelleDateString);
+                if (!DateTime.TryParseExact(nouvelleDateString, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out nouvelleDate))
+                {
+                    await DisplayAlert("Erreur", "La date entrée n'est pas valide.", "OK");
+                    return;
+                }
+
+                // Sélectionner une nouvelle équipe A
+                var equipeNoms = viewModel.Equipes.Select(eq => eq.Nom).ToArray();
+                var equipeSelectionneeNom = nouvelleEquipeA?.Nom ?? ""; // Nom de l'équipe actuelle
+                var indexSelectionne = Array.IndexOf(equipeNoms, equipeSelectionneeNom);
+                var equipeSelectionneeNouveau = await DisplayActionSheet("Sélectionner une équipe A", "Annuler", null, equipeNoms);
+
+                if (equipeSelectionneeNouveau == "Annuler")
+                    return;
+
+                nouvelleEquipeA = viewModel.Equipes.FirstOrDefault(eq => eq.Nom == equipeSelectionneeNouveau);
+
+                if (nouvelleEquipeA == null)
+                {
+                    await DisplayAlert("Erreur", "Veuillez sélectionner une équipe A valide.", "OK");
+                    return;
+                }
+
+                // Sélectionner un nouveau score pour l'équipe A
+                string nouveauScoreAString = await DisplayPromptAsync("Modifier Partie", "Entrez le nouveau score de l'équipe A :", initialValue: partie.ScoreA.ToString());
+                if (!int.TryParse(nouveauScoreAString, out nouveauScoreA))
+                {
+                    await DisplayAlert("Erreur", "Le score entré n'est pas valide.", "OK");
+                    return;
+                }
+
+                // Sélectionner une nouvelle équipe B
+                equipeSelectionneeNom = nouvelleEquipeB?.Nom ?? ""; // Nom de l'équipe actuelle
+                indexSelectionne = Array.IndexOf(equipeNoms, equipeSelectionneeNom);
+                equipeSelectionneeNouveau = await DisplayActionSheet("Sélectionner une équipe B", "Annuler", null, equipeNoms);
+
+                if (equipeSelectionneeNouveau == "Annuler")
+                    return;
+
+                nouvelleEquipeB = viewModel.Equipes.FirstOrDefault(eq => eq.Nom == equipeSelectionneeNouveau);
+
+                if (nouvelleEquipeB == null)
+                {
+                    await DisplayAlert("Erreur", "Veuillez sélectionner une équipe B valide.", "OK");
+                    return;
+                }
+
+                // Sélectionner un nouveau score pour l'équipe B
+                string nouveauScoreBString = await DisplayPromptAsync("Modifier Partie", "Entrez le nouveau score de l'équipe B :", initialValue: partie.ScoreB.ToString());
+                if (!int.TryParse(nouveauScoreBString, out nouveauScoreB))
+                {
+                    await DisplayAlert("Erreur", "Le score entré n'est pas valide.", "OK");
+                    return;
+                }
+
+                viewModel.ModifierPartie(partie, nouvelleDate, nouvelleEquipeA, nouveauScoreA, nouvelleEquipeB, nouveauScoreB);
+            }
+        }
+
     }
 }
